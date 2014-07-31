31a07552fb59b3da9b95720481fd8857faa2cd42
Georgiy <goshazvir@gmail.com>
2014-07-31 11:24:23 +0300

    upd

--- js/init.js
+++ js/init.js
@@ -1,1 +1,1 @@
-head.load("js/lib/jquery.js", "js/lib/baron.full.js", "js/lib/jquery.fancybox.js", "js/common.js");
+head.load("js/lib/jquery.js", "js/lib/baron.full.js", "js/lib/jquery.fancybox.js", "js/lib/cycle.js", "js/common.js");
