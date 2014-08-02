9933fa47188064de4663a0ff5ababb6864301f7a
Georgiy <goshazvir@gmail.com>
2014-08-02 14:24:04 +0300

    fix

--- js/common.js
+++ js/common.js
@@ -38,26 +38,30 @@ head.ready(function() {
 	$('.js-send').on('click', function() {
 	 $(".overlay").addClass("is-active");
 	 $(".popup_send").addClass("is-active");
-	 // return false;
+	 return false;
 	});
 
 	$('.js-cross').on('click', function() {
 		$(".popup").removeClass("is-active");
 		$(".overlay").removeClass("is-active");
+		return false;
 	});
 
 	$('.js-succes').on('click', function() {
 		$(".overlay").addClass("is-active");
 		$(".popup_succes").addClass("is-active");
+		return false;
 	});
 
 	$('.js-gallery').on('click', function() {
 		$(".overlay").addClass("is-active");
 		$(".popup_gallery").addClass("is-active");
+		return false;
 	});
 
 	$('.js-text').on('click', function() {
 		$(".overlay").addClass("is-active");
 		$(".popup_text").addClass("is-active");
+		return false;
 	});
 });
